bd8e4d1d64194a595b3603b3da42356f0cfa8564
Aswad129 <muhammadajaraswadaswad@gmail.com>
2026-02-28 23:03:35 +0800

    Finish Post Test 1

---
+++ kelas/pertemuan-1/petemuan1.cpp
@@ -0,0 +1,42 @@
+// // #include <iostream>
+// // using namespace std;
+
+// // int main()
+// // {
+// //     bool mengantuk = true;
+// //     if (mengantuk)
+// //     {
+// //         cout << "Saya tidur";
+// //     }
+// //     return 0;
+// // }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << i << endl;
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool malas = true;
+    bool mengantuk = true;
+    if (malas)
+    {
+        if (mengantuk)
+        {
+            cout << "Saya tidur";
+        }
+    }
+    else
+    {
+        cout << "Gak ngapa-ngapain";
+    }
+}
